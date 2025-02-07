9aff2b7b5818c70be72c9d9b1837db45040144d3
Vitor P <vitinhocruz64@gmail.com>
2025-02-07 16:39:38 -0300

    feat: coloca o lombok na classe para abstracao

---
+++ src/main/java/dev/Java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -0,0 +1,28 @@
+package dev.Java10x.CadastroDeNinjas.Missoes;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class MissoesModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    String nome_missao;
+    String rank;
+    //UMA MISSAO PODE TER VARIOS NINJAS
+    @OneToMany(mappedBy = "missoes")
+    List<MissoesModel> missoes;
+
+
+}
--- src/main/java/dev/Java10x/CadastroDeNinjas/Ninjas/Controller/NinjaModel.java
+++ src/main/java/dev/Java10x/CadastroDeNinjas/Ninjas/Controller/NinjaModel.java
@@ -2,11 +2,18 @@ package dev.Java10x.CadastroDeNinjas.Ninjas.Controller;
 
 import dev.Java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_Cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class NinjaModel {
 
         @Id
@@ -15,48 +22,9 @@ public class NinjaModel {
         private String nome;
         private String email;
         private int idade;
-        private List<MissoesModel> missoes;
+        //UM NINJA TEM UMA UNICA MISSAO
+        @ManyToOne
+        @JoinColumn(name = "missoes_id")//CHAVE ESTRANGEIRA
+        private MissoesModel missoes;
 
-    public NinjaModel(){}
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    @Override
-    public String toString() {
-        return "NinjaModel{" +
-                "id=" + id +
-                ", nome='" + nome + '\'' +
-                ", email='" + email + '\'' +
-                ", idade=" + idade +
-                ", missoes=" + missoes +
-                '}';
-    }
 }
